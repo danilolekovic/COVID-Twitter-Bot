a6f02409b6e22fd0cf5cff14bf33bcebe25b42b4
Danilo Lekovic <danilo@lekovic.ca>
2020-07-18 13:29:11 -0700

    Initial commit for GitHub

---
+++ README.md
@@ -0,0 +1,11 @@
+# COVID [![Twitter](http://i.imgur.com/tXSoThF.png)](https://twitter.com/CanadaCovid19B1) Bot
+
+This repository contains a Twitter bot that tweets the latest information about the COVID-19 pandemic in Canada. It uses [twit](https://github.com/ttezel/twit#readme) and [disease.sh](https://github.com/disease-sh/api). The part of the bot that tweets new information is in [this file](bot.js), and the part of the bot that retweet Prime Minister Trudeau's latest COVID-related tweet is in [this file](retweet.js).
+
+The bot's twitter account is [@CanadaCovid19B1](https://twitter.com/CanadaCovid19B1).
+
+## Your Country
+
+Want to make the same thing for your country? Fork the repository. Go to [bot.js](bot.js) and, on `line #19`, change **"Canada"** to the name of your country. Beforehand, you will need to setup access to the [Twitter API](https://developer.twitter.com/en) and a [Heroku](https://www.heroku.com) application. Additionally, if you want your bot to retweet your goverment's latest COVID-related tweet, change **"CanadianPM"** on `line #11` of [retweet.js](retweet.js to your government/government leader's Twitter username.
+
+Lots of love from Canada. :heart:
--- bot.js
+++ bot.js
@@ -2,117 +2,83 @@ let Twit = require("twit");
 const https = require("https");
 
 let T = new Twit({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+	consumer_key: process.env.BOT_CONSUMER_KEY,
+	consumer_secret: process.env.BOT_CONSUMER_SECRET,
+	access_token: process.env.BOT_ACCESS_TOKEN,
+	access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
 let thankingPhrases = [
-    "Thank you to the healthcare workers!",
-    "Good work healthcare workers!",
-    "To our frontline healthcare workers, thank you!",
-    "This wouldn't be possible without our healthcare workers!",
-    "We appreciate you, healthcare workers!"
+	"Thank you to the healthcare workers!",
+	"Good work healthcare workers!",
+	"To our frontline healthcare workers, thank you!",
+	"This wouldn't be possible without our healthcare workers!",
+	"We appreciate you, healthcare workers!"
 ];
-/*
-https.get("https://corona.lmao.ninja/v2/countries/Canada", (res) => {
-    let body = "";
-
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
-
-    res.on("end", () => {
-        try {
-            let data = JSON.parse(body);
-
-            Bot.tweet("Currently, there are a total of " + data.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " confirmed cases and " + data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19");
-            Bot.tweet("At the moment, there have been " + data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " recoveries in #Canada. " + thankingPhrases[Math.floor(Math.random() * thankingPhrases.length)] + " 🙏 #COVID19");
-            Bot.tweet("Today, there have been " + data.todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " new confirmed cases and " + data.todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19");
-            Bot.tweet("At the moment, there have been " + data.tests.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " tests for #COVID19 in #Canada");
-        
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
-
-}).on("error", (error) => {
-    console.error(error.message);
-});
-*/
-/*
-T.post('media/upload', { media_data: "" }, function (err, data, response) {
-    if (err) {
-        console.log(err)
-    } else {
-        const status = {
-            status: "Today's chart of #Canada's progress with #COVID19.",
-            media_ids: media.media_id_string
-        }
-        
-        T.post("statuses/update", status, function(error, tweet, response) {
-            if (error) {
-            console.log(error)
-            } else {
-            console.log("Successfully tweeted an image!")
-            }
-        })
-    }
-});*/
-
 
 https.get("https://corona.lmao.ninja/v2/countries/Canada", (res) => {
-    let body = "";
+	let body = "";
 
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
+	res.on("data", (chunk) => {
+		body += chunk;
+	});
 
-    res.on("end", () => {
-        try {
-            let data = JSON.parse(body);
+	res.on("end", () => {
+		try {
+			let data = JSON.parse(body);
 
-            T.post('statuses/update', 
-                {status: "Currently, there are a total of " + data.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " confirmed cases and " + data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19"}, 
-                function(error, tweet, response){ 
-                    if(error){ console.log(error); } 
-                        console.log(tweet); 
-                        console.log(response); 
-                }
-            );
+			T.post("statuses/update", {
+					status: "Currently, there are a total of " + data.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " confirmed cases and " + data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19"
+				},
+				function(error, tweet, response) {
+					if (error) {
+						console.log(error);
+					}
+					console.log(tweet);
+					console.log(response);
+				}
+			);
 
-            T.post('statuses/update', 
-                {status: "At the moment, there have been " + data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " recoveries in #Canada. " + thankingPhrases[Math.floor(Math.random() * thankingPhrases.length)] + " 🙏 #COVID19"}, 
-                function(error, tweet, response){ 
-                    if(error){ console.log(error); } 
-                        console.log(tweet); 
-                        console.log(response); 
-                }
-            );
+			T.post("statuses/update", {
+					status: "At the moment, there have been " + data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " recoveries in #Canada. " + thankingPhrases[Math.floor(Math.random() * thankingPhrases.length)] + " 🙏 #COVID19"
+				},
+				function(error, tweet, response) {
+					if (error) {
+						console.log(error);
+					}
+					console.log(tweet);
+					console.log(response);
+				}
+			);
 
-            T.post('statuses/update', 
-                {status: "Today, there have been " + data.todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " new confirmed cases and " + data.todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19"}, 
-                function(error, tweet, response){ 
-                    if(error){ console.log(error); } 
-                        console.log(tweet); 
-                        console.log(response); 
-                }
-            );
+			T.post("statuses/update", {
+					status: "Today, there have been " + data.todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " new confirmed cases and " + data.todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " deaths in #Canada. #COVID19"
+				},
+				function(error, tweet, response) {
+					if (error) {
+						console.log(error);
+					}
+					console.log(tweet);
+					console.log(response);
+				}
+			);
 
-            T.post('statuses/update', 
-                {status: "At the moment, there have been " + data.tests.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " tests for #COVID19 in #Canada"}, 
-                function(error, tweet, response){ 
-                    if(error){ console.log(error); } 
-                        console.log(tweet); 
-                        console.log(response); 
-                }
-            );
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
+			T.post("statuses/update", {
+					status: "At the moment, there have been " + data.tests.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " tests for #COVID19 in #Canada"
+				},
+				function(error, tweet, response) {
+					if (error) {
+						console.log(error);
+					}
+					console.log(tweet);
+					console.log(response);
+				}
+			);
+		} catch (error) {
+			console.error(error.message);
+		};
+	});
 
 }).on("error", (error) => {
-    console.error(error.message);
+	console.error(error.message);
 });
